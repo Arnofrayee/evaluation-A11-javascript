b60908012b10310a9113c4e9175b7d9f0925f21e
Arnofraye <aboudissa@edenschool.fr>
2025-11-04 15:19:25 +0100

    Update 07

--- ex07.js
+++ ex07.js
@@ -5,10 +5,10 @@ function getReduction(price, age) {
 		return 0;
 	}
 	if (age > 5 && age <= 18) {
-		return price * 0.5;
+		return price - price * 0.5;
 	}
 	if (age > 18 && age <= 25) {
-		return price * 0.3;
+		return price - price * 0.3;
 	}
 	if (age > 25) {
 		return price;
